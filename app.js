64008b874c6df5e6ee605d1011715c3b6a121e7b
Nackative <53878504+Nackative@users.noreply.github.com>
2023-05-21 08:28:09 +0700

    Update app.js  3

--- app.js
+++ app.js
@@ -2,22 +2,16 @@ const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const res = require('express/lib/response');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5050;
-
+const port = 5050;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
-
-app.set("view",".NODE.JS/app.js");
-app.set("app.js engine", "ejs")
-
 app.get("/", (req, res) =>{
 
-    res.render('index', {username: 'Snack', customer:["Nakative16", "Sapphaya", "Kingdom"]});
+    res.send('Hi NS Family Company 2022');
 
 }) 
 
